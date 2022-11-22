bff9217b7932f8c2900c9362f55d4bcd6ca83b48
kurotom <55354389+kurotom@users.noreply.github.com>
2022-11-22 12:08:31 -0300

    proyecto 1 terminado

---
+++ app.js
@@ -0,0 +1,45 @@
+let express = require("express");
+let app = express();
+
+
+
+app.get('/', (req, res) => {
+  let absPath = __dirname + '/views/index.html';
+  res.sendFile(absPath);
+});
+
+app.get('/api/:time', (req, res) => {
+  let query = req.params.time;
+  let date;
+
+  if (query.length === 10) {
+    date = new Date(query);
+    console.log(query, date);
+    if (date.toString()  !== 'Invalid Date') {
+      res.json({
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+      });
+    } else {
+      res.json({"error": "Invalid Date"});
+    }
+  } else if (query.length === 13) {
+    date = new Date(parseInt(query));
+    reg = /\d+/;
+    match = query.match(reg)[0].length;
+    if (match === 13) {
+      res.json({
+        "unix": date.getTime(),
+        "utc": date.toUTCString()
+      });
+    } else {
+      res.json({"error": "Invalid Date"})
+    }
+  } else {
+    res.json({"error": "Invalid Date"})
+  }
+});
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,18 @@
+let app = require('./app.js');
+
+let express = require('express');
+
+let server = express();
+
+let port = process.env.PORT || 3000;
+
+server.use('/', app)
+
+server.listen(port, (error) => {
+  if (!error) {
+    console.log(`App listening on port ${port}`);
+    console.log(`Go to url: http://127.0.0.1:${port}\n`);
+  } else {
+    console.log(`Server can't start.\n${error}`);
+  }
+});
